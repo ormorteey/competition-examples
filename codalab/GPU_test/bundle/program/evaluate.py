4aef4e7b0ef2cee6f39833a4083b1c2e0a2ebc40
didayolo <adrien.pavao@gmail.com>
2023-02-04 03:38:28 +0100

    Fix GPU test template

--- codalab/GPU_test/bundle/program/evaluate.py
+++ codalab/GPU_test/bundle/program/evaluate.py
@@ -16,20 +16,21 @@ output_dir = sys.argv[2]
 submit_dir = os.path.join(input_dir, 'res')
 truth_dir = os.path.join(input_dir, 'ref')
 
-# The submission
-print('Initialize the submission')
-sys.path.append(submit_dir)
-from submission import Model
-model = Model() # <--- torch.cuda.is_available() run here
-
 if os.path.isdir(submit_dir) and os.path.isdir(truth_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
     # Output file
     output_file = open(os.path.join(output_dir, 'scores.txt'), 'w')
-    # Check GPU using PyTorch
-    passing = model.is_gpu_available()
-    output_file.write("passed: {}".format(int(passing)))
+    # Check GPU using PyTorch (output of the submission)
+    print('Reading the result of the test (from the submission)')
+    test_result_file = os.path.join(submit_dir, 'answer.txt')
+    if os.path.exists(test_result_file):
+        f = open(test_result_file, 'r')
+        gpu_available = int(f.read())
+        f.close()
+    # Write score for leaderboard
+    result = "gpu_available: {}".format(gpu_available)
+    print(result)
+    output_file.write(result)
     output_file.close()
-
 print('End of program')
--- codalab/GPU_test/bundle/submission/submission.py
+++ codalab/GPU_test/bundle/submission/submission.py
@@ -2,13 +2,23 @@
 # - submission.py (this file)
 # - metadata file
 
+# To understand how this code submission is executed, refer to:
+# https://github.com/codalab/codalab-competitions/wiki/User_Building-an-Ingestion-Program-for-a-Competition#execution-priority
+
 # Import
 import torch
+import os
+import sys
 
-class Model:
-    def __init__(self):
-        # True or False if GPU available or not
-        self.passing = torch.cuda.is_available()
-
-    def is_gpu_available(self):
-        return self.passing
+if __name__=="__main__":
+    # Paths
+    input_dir = os.path.abspath(sys.argv[1])
+    output_dir = os.path.abspath(sys.argv[2])
+    print("Using input_dir: " + input_dir)
+    print("Using output_dir: " + output_dir)
+    # True or False if GPU available or not
+    gpu_available = torch.cuda.is_available()
+    print('torch.cuda.is_available() --> {}'.format(gpu_available))
+    # Write test result
+    with open(os.path.join(output_dir, 'answer.txt'), 'w') as result_file:
+        result_file.write(str(int(gpu_available)))
